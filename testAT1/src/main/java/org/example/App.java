b1822bff8d63b98956de5923a8c8b4ad1c9e22d9
TamilaNapova <113239014+TamilaNapova@users.noreply.github.com>
2024-07-29 14:56:39 +0300

    added App files

---
+++ testAT1/src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        String s1 = "eee", s2 = "eee";
+        if (s1.equals(s2)){
+            System.out.println(s1 + " and "+ s2 + " teski");
+
+        }
+    }
+}
